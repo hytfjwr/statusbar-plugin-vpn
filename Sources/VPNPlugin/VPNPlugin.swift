ecd62c6a3cd74834bcbc347146b051f54957ead8
hytfjwr <133951156+hytfjwr@users.noreply.github.com>
2026-03-18 12:03:15 +0900

    Add @_cdecl plugin factory for dylib loading

--- Sources/VPNPlugin/VPNPlugin.swift
+++ Sources/VPNPlugin/VPNPlugin.swift
@@ -13,3 +13,11 @@ public struct VPNPlugin: StatusBarPlugin {
         widgets = [VPNWidget()]
     }
 }
+
+// MARK: - Plugin Factory
+
+@_cdecl("createStatusBarPlugin")
+public func createStatusBarPlugin() -> UnsafeMutableRawPointer {
+    let box = PluginBox { VPNPlugin() }
+    return Unmanaged.passRetained(box).toOpaque()
+}
